26ec2c7b9dab117d88871bd1dd254a9cb0ca2ca7
tegnike <o3nike.teg.14@gmail.com>
2023-07-25 01:03:21 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+client = MyClient()
+client.run('MTEzMzE2MTU4MjQ1MzYwMDM3Nw.GtaoW-.5b5CtC0eXkKxSP2NPMgvy85EC7VeZUYUsmaxjk')
