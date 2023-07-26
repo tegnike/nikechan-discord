902ce0b7df32686a0713e38e296b52594ae202b3
tegnike <o3nike.teg.14@gmail.com>
2023-07-26 21:24:44 +0200

    Add OpenAI Response

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import discord
+import os
+from datetime import datetime, timedelta
+from pytz import timezone
+from langchain.memory import ChatMessageHistory
+from .services import openai_service
 
 intents = discord.Intents.all()
+discord_key = os.environ['DISCORD_KEY']
 
 class MyClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.history = ChatMessageHistory()
+        self.count = 0
+        self.current_date = datetime.now(timezone('Europe/Warsaw')).date()
+
     async def on_ready(self):
         print('Bot is ready.')
         print('Logged in as', self.user)
 
     async def on_message(self, message):
-        print('Message object:', message)  # メッセージオブジェクト全体を出力
         print('Message received from', message.author, ':', message.content)
 
         # don't respond to ourselves
@@ -16,11 +27,23 @@ class MyClient(discord.Client):
             print('Message received from self, ignoring.')
             return
 
-        if message.content == 'ping':
-            print('Ping received, sending pong.')
-            await message.channel.send('pong')
+        if 'AIニケちゃん' in message.content:
+            print('Message contains "AIニケちゃん".')
+
+            # Check if date has changed
+            new_date = datetime.now(timezone('Europe/Warsaw')).date()
+            if new_date > self.current_date:
+                self.count = 0
+                self.current_date = new_date
+
+            model_name = "gpt-4" if self.count <= 20 else "gpt-3.5-turbo"
+            response = get_openai_response(message.content, self.history, model_name)
+            await message.channel.send(response)
+
+            self.count += 1
+            print('Message send completed.')
         else:
             print('Message not recognized.')
 
 client = MyClient(intents=intents)
-client.run('MTEzMzE2MTU4MjQ1MzYwMDM3Nw.GiWxQf.g4cnVupSdVI5YmZBmqlDkkpMmKmJqjXjYVuIzA')
+client.run(discord_key)
---
+++ openai_service.py
@@ -0,0 +1,35 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage,
+)
+
+def get_system_message():
+    with open('app/services/system_message.txt', 'r') as file:
+        return file.read().strip()
+
+def get_openai_response(message, history, model_name):
+    # 過去10件のメッセージを取得
+    latest_messages = history.messages[-10:]  
+    past_messages = []
+    for latest_message in latest_messages:
+        if isinstance(latest_message, HumanMessage):
+            past_messages.append(HumanMessage(content=latest_message.content))
+        else:
+            past_messages.append(AIMessage(content=latest_message.content))
+
+    # 会話履歴を更新
+    history.add_user_message(message)
+    print("User:", message)
+
+    # OpenAIによる応答生成
+    messages = [SystemMessage(content=get_system_message())] + latest_messages + [HumanMessage(content=message)]
+    chat = ChatOpenAI(model_name=model_name, temperature=0)
+    response = chat(messages)
+
+    # 会話履歴を更新
+    history.add_ai_message(response.content)
+    print("AI:", response.content)
+
+    return response.content
