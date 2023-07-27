7ecc2ffca5cdeba24c5ef1c371d490c3fdb86e44
tegnike <o3nike.teg.14@gmail.com>
2023-07-27 09:56:33 +0200

    Add conversation recogination

--- main.py
+++ main.py
@@ -11,41 +11,53 @@ discord_key = os.environ['DISCORD_KEY']
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.history = ChatMessageHistory()
-        self.count = 0
-        self.current_date = datetime.now(timezone('Europe/Warsaw')).date()
+        self.states = {}
 
     async def on_ready(self):
         print('Bot is ready.')
         print('Logged in as', self.user)
 
     async def on_message(self, message):
+        # Get the server's ID
+        server_id = message.guild.id
+
+        # If this server's state is not initialized yet, initialize it
+        if server_id not in self.states:
+            self.states[server_id] = {
+                "history": ChatMessageHistory(),
+                "count": 0,
+                "current_date": datetime.now(timezone('Europe/Warsaw')).date(),
+            }
+
+        # Get this server's state
+        state = self.states[server_id]
+
         print('Message received from', message.author.nick, ':', message.content)
 
         # don't respond to ourselves
         if message.author == self.user:
             print('Message received from self, ignoring.')
             return
-        if self.count > 100:
+        if state["count"] > 100:
             print('Message limit.')
             return
 
         # ユーザーメッセージを会話履歴に追加
-        self.history.add_user_message(message.author.nick + ": " + message.content)
+        state["history"].add_user_message(message.author.nick + ": " + message.content)
         print("User:", message.content)
 
-        if judge_if_i_response(self.history):
+        if judge_if_i_response(state["history"]):
             # Check if date has changed
             new_date = datetime.now(timezone('Europe/Warsaw')).date()
-            if new_date > self.current_date:
-                self.count = 0
-                self.current_date = new_date
+            if new_date > state["current_date"]:
+                state["count"] = 0
+                state["current_date"] = new_date
 
-            model_name = "gpt-4" if self.count <= 20 else "gpt-3.5-turbo"
-            response = get_openai_response(self.history, model_name)
+            model_name = "gpt-4" if state["count"] <= 20 else "gpt-3.5-turbo"
+            response = get_openai_response(state["history"], model_name)
             await message.channel.send(response)
 
-            self.count += 1
+            state["count"] += 1
             print('Message send completed.')
         else:
             print('Message not recognized.')
